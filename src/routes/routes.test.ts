6827813fb6cc0c84d60ce030e6c054fb49fbe63b
Ayrton <ayrtong.guerra@gmail.com>
2023-10-05 04:07:48 -0400

    chore: new name test routes

---
+++ src/routes/routes.test.ts
@@ -0,0 +1,22 @@
+import { beforeAll, describe, expect, it } from "bun:test"
+import request from 'supertest'
+import app from '../app'
+import User from "../models/User"
+
+describe('Testing api routes',() => {
+
+    // beforeAll(async () => {
+    //     await User.deleteMany({})
+    // })
+
+    it('should ping pong', (done) => {
+        request(app)
+            .get('/ping')
+            .then(response => {
+                expect(response.body.pong).toBeTruthy()
+                return done()
+            })
+    })
+})
+
+// TODO: remember to make all endpoints test 

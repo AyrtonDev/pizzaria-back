4c5fbc22b6051ca1f4b36a7f3c0fd21cecf822c5
Ayrton <ayrtong.guerra@gmail.com>
2023-10-05 04:07:16 -0400

    chore: inclued middlewares in the routes

--- src/routes/pizzas.ts
+++ src/routes/pizzas.ts
@@ -1,16 +1,17 @@
 import { Router } from 'express'
 import * as PizzaController from '../controllers/pizzas.controllers'
+import { Auth } from '../middleware/auth'
 
 const router = Router()
 
 router.get('/pizzas', PizzaController.all)
 
-router.get('/pizza/:id', PizzaController.one)
+router.get('/pizza/:id', Auth.private, PizzaController.one)
 
-router.post('/pizza', PizzaController.create)
+router.post('/pizza', Auth.private, PizzaController.create)
 
-router.put('/pizza/:id', PizzaController.update)
+router.put('/pizza/:id', Auth.private, PizzaController.update)
 
-router.delete('/pizza/:id', PizzaController.remove)
+router.delete('/pizza/:id', Auth.private, PizzaController.remove)
 
 export default router
